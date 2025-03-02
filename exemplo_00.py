1c10784be8665fa77c4b3533bf5905bccb4d2fbc
Thales Felix <tbfelix00@outlook.com>
2025-03-02 11:30:41 -0300

    modificações

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,7 @@
-print ("Hello World")
+# Criar programa que o usuário digita o seu nome e retorna o número de caracteres
+
+
+print(len(input("Digite seu nome: ")))
+
+
+
---
+++ exemplo_01.py
@@ -0,0 +1,8 @@
+print(int(input("Digite primeiro valor: ")) + int(input("Digite segundo valor: ")))
+
+# Tipos primitivos
+numero = 3 #int
+print(type(numero))   # tipagem dos dados
+numero_decimal = 4.78 #float
+nome_de_usuário = "Luciano" #string
+verdadeiro = True #boolean
